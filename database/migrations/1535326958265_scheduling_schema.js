f83d1de4d2c6dfdbec49521a56735b8f267048c5
Jean Cabral <jeancabral@gmail.com>
2018-08-26 20:49:26 -0300

    WIP - CRUD Scheduling

--- database/migrations/1535326958265_scheduling_schema.js
+++ database/migrations/1535326958265_scheduling_schema.js
@@ -6,6 +6,22 @@ class SchedulingSchema extends Schema {
   up () {
     this.create('schedulings', (table) => {
       table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('destino').notNullable()
+      table.string('finalidade').notNullable()
+      table.text('observacao').notNullable()
+      table.integer('qtdPassageiros').notNullable()
+      table.string('cargaTransportada').notNullable()
+      table.date('dataPartida').notNullable()
+      table.date('dataRetorno').notNullable()
+      table.time('horaPartida').notNullable()
+      table.time('horaRetorno').notNullable()
       table.timestamps()
     })
   }
