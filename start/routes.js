b7e48fc189cd95e8ce176c3ca037a3d72b443dbc
Jean Cabral <jeancabral@gmail.com>
2018-08-26 20:36:47 -0300

    Autenticação JWT e API REST

--- start/routes.js
+++ start/routes.js
@@ -15,6 +15,6 @@
 
 const Route = use('Route')
 
-Route.get('/', ({ request }) => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('/users', 'UserController.create')
+
+Route.post('/sessions', 'SessionController.create')
