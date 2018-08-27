7e55dc1d7c016e9def16b5340b9ced1115fc55ea
Jean Cabral <jeancabral@gmail.com>
2018-08-27 17:31:29 -0300

    WIP - CRUD Scheduling and Git Flow

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -9,6 +9,12 @@ class UserSchema extends Schema {
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.string('name', 60).notNullable()
+      table.string('lastname', 60).notNullable()
+      table.enu('role', ['admin', 'driver','passenger'])
+      table.string('department', 60).notNullable()
+      table.string('enrollment', 4).notNullable()
+      table.string('telephone', 60).notNullable()
       table.timestamps()
     })
   }
