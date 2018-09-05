e35400cd591b29015230a3bd0420768971436019
Jean Cabral <jeancabral@gmail.com>
2018-09-04 21:37:06 -0300

    WIP-Csv

--- app/Controllers/Http/CsvController.js
+++ app/Controllers/Http/CsvController.js
@@ -4,9 +4,6 @@ const Helpers = use('Helpers')
 
 const Csv = use('App/Models/Csv')
 
-var fs = require('fs')
-
-
 /**
  * Resourceful controller for interacting with csvs
  */
@@ -26,29 +23,11 @@ class CsvController {
       size: '2mb'
     })
 
-
-    // await profilePic.move(Helpers.tmpPath('uploads'), {
-    //   name: `${Date.now()}-${this._clientName}`
-    // })
-
-    // if (!profilePic.moved()) {
-    //   return profilePic.error()
-    // }
-
     await profilePic.moveAll(Helpers.tmpPath('uploads'), file => ({
       name: `${Date.now()}-${file.clientName}`
       
+      
     }))
-
-    fs.readFile(Helpers.tmpPath('uploads').concat('/1536029295067-domingos%2023082019.txt'), 'utf8', function(err, data)
-    {
-        if (err)
-        {
-            // check and handle err
-        }
-        var linesExceptFirst = data.split('\n').slice(1).join('\n');
-        fs.writeFile(profilePic, linesExceptFirst);
-    });
 
     if (!profilePic.movedAll()) {
       return profilePic.errors()
